e61433f9831d337e0d448078cff253647dc4fb60
Nikolay Bystritskiy <nikolay.bystritskiy@gmail.com>
2023-12-11 20:49:11 +0000

    fix handler. it looks like entity can't be from Ticker_.CDS_name. it should be literal

--- stonks/srv/src/main/java/nbys/stonks/eventhandlers/TickerEventHandler.java
+++ stonks/srv/src/main/java/nbys/stonks/eventhandlers/TickerEventHandler.java
@@ -2,28 +2,24 @@ package nbys.stonks.eventhandlers;
 
 import org.springframework.stereotype.Component;
 
-import com.sap.cds.services.EventContext;
+import com.sap.cds.services.cds.CdsReadEventContext;
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.After;
 import com.sap.cds.services.handler.annotations.ServiceName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import cds.gen.myservice.MyService;
 import cds.gen.myservice.MyService_;
-import cds.gen.myservice.Ticker;
-import cds.gen.nbys.stonks.Ticker_;
 
 @Component
 @ServiceName(MyService_.CDS_NAME)
 public class TickerEventHandler implements EventHandler {
     static final Logger logger = LoggerFactory.getLogger(TickerEventHandler.class);
 
-    @After(entity = Ticker_.CDS_NAME, event = MyService.EVENT_READ)
-    public void onReadTicker(EventContext req) {
-        System.out.println("HAHAHAHAHA" + req.toString());
-        logger.info("HAHAHAHAHA" + req.toString());
+    @After(event = CqnService.EVENT_READ, entity = "MyService.Ticker")
+    public void onReadTicker(CdsReadEventContext req) {
+        logger.info(req.getEvent() + req.getTarget().toString());
+        logger.info(req.getResult().toString());
         req.setCompleted();
     }
 }
