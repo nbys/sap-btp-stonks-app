5c1020e1bc4eecc77e167fef1dbac47603ccf62b
Nikolay Bystritskiy <nikolay.bystritskiy@gmail.com>
2023-12-13 11:29:02 +0000

    explore more on event types, reading data using cqn query from persistent store

--- stonks/srv/src/main/java/nbys/stonks/eventhandlers/TickerEventHandler.java
+++ stonks/srv/src/main/java/nbys/stonks/eventhandlers/TickerEventHandler.java
@@ -1,12 +1,19 @@
 package nbys.stonks.eventhandlers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Component;
 
+import com.sap.cds.Result;
 import com.sap.cds.services.cds.CdsReadEventContext;
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.After;
+import com.sap.cds.services.handler.annotations.Before;
+import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.persistence.PersistenceService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import cds.gen.myservice.MyService_;
@@ -17,10 +24,22 @@ import cds.gen.myservice.Ticker_;
 public class TickerEventHandler implements EventHandler {
     static final Logger logger = LoggerFactory.getLogger(TickerEventHandler.class);
 
-    @After(event = CqnService.EVENT_READ, entity = Ticker_.CDS_NAME)
-    public void onReadTicker(CdsReadEventContext req) {
+    @Autowired
+    private PersistenceService db;
+
+    @On(event = CqnService.EVENT_READ, entity = Ticker_.CDS_NAME)
+    public Result onReadTicker(
+            CdsReadEventContext req) {
         logger.info(req.getEvent() + req.getTarget().toString());
-        logger.info(req.getResult().toString());
-        req.setCompleted();
+        // logger.info(req.getResult().toString());
+        logger.info(req.getCqn().toString());
+
+        Result res = db.run(req.getCqn());
+
+        res.forEach(row -> {
+            logger.info(row.toString());
+            row.put("name", String.format("%s - %S", row.get("name"), row.get("symbol")));
+        });
+        return res;
     }
 }
